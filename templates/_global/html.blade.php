18c68d4d0d9eae46cb08f8175b4552fc58033847
Simon Asika <asika32764@gmail.com>
2025-02-24 23:36:21 +0800

    Update html.blade.php

--- templates/_global/html.blade.php
+++ templates/_global/html.blade.php
@@ -71,6 +71,8 @@
     </div>
     @show
 
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="asika32764" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+
 {!! $asset->getTemplate()->renderTemplates() !!}
 </body>
 </html>
